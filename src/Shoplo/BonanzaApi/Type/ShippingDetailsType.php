b0f96d85d923797ba9cd0348aa7e1c972cf5e0c9
Paul P <bigpaulie25ro@yahoo.com>
2018-02-14 15:48:01 +0200

    Refactoring ShippingDetailsType

--- src/Shoplo/BonanzaApi/Type/ShippingDetailsType.php
+++ src/Shoplo/BonanzaApi/Type/ShippingDetailsType.php
@@ -8,40 +8,40 @@ use JMS\Serializer\Annotation as Serializer;
 
 class ShippingDetailsType
 {
-	/**
-	 * @var double
-	 *
-	 * @Serializer\Type("double")
-	 */
-	public $amount;
+    /**
+     * @var InsuranceDetailsType
+     *
+     * @Serializer\Type("Shoplo\BonanzaApi\Type\InsuranceDetailsType")
+     */
+    public $insuranceDetails;
 
-	/**
-	 * @var double
-	 *
-	 * @Serializer\Type("double")
-	 */
-	public $insuranceFee;
+    /**
+     * @var string
+     *
+     * Allowed values in \Shoplo\BonanzaApi\Enums\ShippingServiceType
+     *
+     * @Serializer\Type("string")
+     */
+    public $shippingService;
 
-	/**
-	 * @var string
-	 *
-	 * @Serializer\Type("string")
-	 */
-	public $notes;
+    /**
+     * @var CalculatedShippingRateType
+     *
+     * @Serializer\Type("Shoplo\BonanzaApi\Type\CalculatedShippingRateType")
+     */
+    public $calculatedShippingRate;
 
-	/**
-	 * @var string[]
-	 *
-	 * @Serializer\Type("array<string>")
-	 */
-	public $servicesArray;
+    /**
+     * @var InternationalShippingServiceOptionType
+     *
+     * @Serializer\Type("Shoplo\BonanzaApi\Type\InternationalShippingServiceOptionType")
+     */
+    public $internationalShippingServiceOption;
 
-	/**
-	 * @var string
-	 *
-	 * Allowed values in \Shoplo\BonanzaApi\Enums\ShippingServiceType
-	 *
-	 * @Serializer\Type("string")
-	 */
-	public $shippingService;
+    /**
+     * @var ShippingServiceOptionsType
+     *
+     * @Serializer\Type("Shoplo\BonanzaApi\Type\ShippingServiceOptionsType")
+     */
+    public $shippingServiceOptions;
 }
